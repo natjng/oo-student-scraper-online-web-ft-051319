2c6e5cb9999bc7f055c17366d88ed8f7b417c573
Natalie J <natsetc@gmail.com>
2019-06-06 06:23:57 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,12 +3,61 @@ require 'pry'
 
 class Scraper
 
+# take in an argument of the URL of the index page
+# the URL will be "./fixtures/student-site/index.html". 
+# It should use Nokogiri and Open-URI to access that page. 
+# The return value of this method should be an array of hashes in which each hash represents a single student. The keys of the individual student hashes should be :name, :location and :profile_url
+# => [
+# {:name => "Abby Smith", :location => "Brooklyn, NY", :profile_url => "students/abby-smith.html"}, 
+# {:name => "Joe Jones", :location => "Paris, France", :profile_url => "students/joe-jonas.html"}
+# ]
+
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    student_site = Nokogiri::HTML(html)
+    students = []
+    student_site.css("div.roster-cards-container").each do |card|
+      card.css(".student-card a").each do |student|
+        student_name = student.css(".student-name").text
+        student_location = student.css(".student-location").text
+        url = student.attribute("href").value
+        students << {name: student_name, location: student_location, profile_url: url}
+      end
+    end
+    students
   end
 
+
+# take in an argument of a student's profile URL. 
+# It should use Nokogiri and Open-URI to access that page. 
+# The return value of this method should be a hash in which the key/value pairs describe an individual student. 
+# Some students dont have a twitter or some other social link. Be sure to be able to handle that.
+  # => {:twitter=>"http://twitter.com/flatironschool",
+  #     :linkedin=>"https://www.linkedin.com/in/flatironschool",
+  #     :github=>"https://github.com/learn-co,
+  #     :blog=>"http://flatironschool.com",
+  #     :profile_quote=>"\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi",
+  #     :bio=> "I'm a school"
+  #   }
+
   def self.scrape_profile_page(profile_url)
-    
+    student_profile = Nokogiri::HTML(open(profile_url))
+    student = {}
+    links = student_profile.css("div.social-icon-container a").map{|link| link.attribute("href").value}
+    links.each do |link|
+      if link.include?("twitter")
+        student[:twitter] = link
+      elsif link.include?("linkedin")
+        student[:linkedin] = link
+      elsif link.include?("github")
+        student[:github] = link
+      else
+        student[:blog] = link
+      end
+    end
+    student[:profile_quote] = student_profile.css("div.profile-quote").text if student_profile.css("div.profile-quote")
+    student[:bio] = student_profile.css("div.description-holder p").text if student_profile.css("div.description-holder p")
+    student
   end
 
 end
--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,34 @@ class Student
   @@all = []
 
   def initialize(student_hash)
+    # take in an argument of a hash and use metaprogramming to assign the newly created student attributes and values in accordance with the key/value pairs of the hash
+    # Use the #send method to achieve this. 
     
+    student_hash.each {|k,v| self.send(("#{k}="), v)}
+    @@all << self
   end
 
+
+  # This class method should take in an array of hashes. 
+  # Call Student.create_from_collection with the return value of the Scraper.scrape_index_page method as the argument.
+  # The method (#create_from_collection) should iterate over the array of hashes and create a new individual student using each hash. 
+ 
   def self.create_from_collection(students_array)
-    
+    students_array.each {|student_hash| self.new(student_hash)}
   end
+
+
+# This instance method should take in a hash whose key/value pairs describe additional attributes of an individual student.
+# Call student.add_student_attributes with the return value of the Scraper.scrape_profile_page method as the argument.
+# method should iterate over the given hash and use metaprogramming to dynamically assign the student attributes and values in accordance with the key/value pairs of the hash. Use the #send method to achieve this.
+# The return value of this method should be the student itself. Use the self keyword.
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each {|k,v| self.send(("#{k}="), v)}
   end
 
   def self.all
-    
+    @@all
   end
 end
 
